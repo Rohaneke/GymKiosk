221f688e384a8f8375b56420c97c63abf2c5b625
Salahedin90 <102454@glr.nl>
2025-11-04 14:48:27 +0000

    gewerkt aan css

--- workout/js/workout.js
+++ workout/js/workout.js
@@ -1,3 +1,18 @@
 function navigateTo(page) {
     window.location.href = page;
 }
+
+function showWorkout(type) {
+    const workoutInfo = document.getElementById('workout-info');
+    let content = '';
+
+    if (type === 'push') {
+        content = '<h3>Push Workout</h3><ul><li>Bench Press</li><li>Overhead Press</li><li>Tricep Dips</li></ul>';
+    } else if (type === 'pull') {
+        content = '<h3>Pull Workout</h3><ul><li>Pull-Ups</li><li>Barbell Rows</li><li>Bicep Curls</li></ul>';
+    } else if (type === 'legs') {
+        content = '<h3>Legs Workout</h3><ul><li>Squats</li><li>Deadlifts</li><li>Lunges</li></ul>';
+    }
+
+    workoutInfo.innerHTML = content;
+}
