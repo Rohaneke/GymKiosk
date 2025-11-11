5c1aee9beb3cfc774784546801e13ba6de7e0e47
Salahedin90 <102454@glr.nl>
2025-11-11 13:10:40 +0000

    workoutpage

--- workout/js/workout.js
+++ workout/js/workout.js
@@ -1,17 +1,37 @@
-function navigateTo(page) {
-    window.location.href = page;
-}
-
 function showWorkout(type) {
-    const workoutInfo = document.getElementById('workout-info');
-    let content = '';
+    const workoutInfo = document.getElementById("workout-info");
+    let content = "";
 
-    if (type === 'push') {
-        content = '<h3>Push Workout</h3><ul><li>Bench Press</li><li>Overhead Press</li><li>Tricep Dips</li></ul>';
-    } else if (type === 'pull') {
-        content = '<h3>Pull Workout</h3><ul><li>Pull-Ups</li><li>Barbell Rows</li><li>Bicep Curls</li></ul>';
-    } else if (type === 'legs') {
-        content = '<h3>Legs Workout</h3><ul><li>Squats</li><li>Deadlifts</li><li>Lunges</li></ul>';
+    if (type === "push") {
+        content = `
+            <h3>Push Day</h3>
+            <ul>
+                <li>Bench Press</li>
+                <li>Overhead Shoulder Press</li>
+                <li>Tricep Dips</li>
+                <li>Lateral Raises</li>
+            </ul>
+        `;
+    } else if (type === "pull") {
+        content = `
+            <h3>Pull Day</h3>
+            <ul>
+                <li>Pull-Ups</li>
+                <li>Barbell Rows</li>
+                <li>Face Pulls</li>
+                <li>Bicep Curls</li>
+            </ul>
+        `;
+    } else if (type === "legs") {
+        content = `
+            <h3>Leg Day</h3>
+            <ul>
+                <li>Squats</li>
+                <li>Romanian Deadlifts</li>
+                <li>Leg Press</li>
+                <li>Calf Raises</li>
+            </ul>
+        `;
     }
 
     workoutInfo.innerHTML = content;
